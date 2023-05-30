b8a672d37667731c47fe1b08d85236dc946b714c
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-30 13:24:15 +0200

    Configuración de Binding MainActivity.kt

--- app/src/main/java/com/example/practicasoporte/MainActivity.kt
+++ app/src/main/java/com/example/practicasoporte/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.practicasoporte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practicasoporte.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //hola
-        //Mi
-        //bebe
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
