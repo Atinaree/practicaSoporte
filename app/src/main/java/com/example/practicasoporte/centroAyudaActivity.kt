80e2a69c453efe5594381fdab78cfbfa50451d57
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-31 08:46:14 +0200

    Creación de botones en la vista de "Centro Actividad" y configuracion de sus recursos

--- app/src/main/java/com/example/practicasoporte/centroAyudaActivity.kt
+++ app/src/main/java/com/example/practicasoporte/centroAyudaActivity.kt
@@ -2,10 +2,15 @@ package com.example.practicasoporte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practicasoporte.databinding.ActivityCentroAyudaBinding
+import com.example.practicasoporte.databinding.ActivityLoginBinding
 
 class centroAyudaActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityCentroAyudaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_centro_ayuda)
+        binding = ActivityCentroAyudaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
