657f15e13a3db5c84fc2b4c63a2db30cf7a0b071
atinaree <christian_cv.90@hotmail.com>
2023-05-31 11:08:45 +0200

    Añadido bot de prueba

---
+++ app/src/main/java/com/example/practicasoporte/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.practicasoporte
+
+object Constants {
+    const val SEND_ID = "SEND_ID"
+    const val RECIVE_ID = "RECIVE_ID"
+    const val OPEN_PAGE = "Dirigiendo al navegador..."
+
+}
