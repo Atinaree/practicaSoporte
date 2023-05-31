358a77ac03a06d62371aa698a9b1a46c36ef7162
atinaree <christian_cv.90@hotmail.com>
2023-05-31 09:07:31 +0200

    Añadido los styles

--- app/src/main/java/com/example/practicasoporte/ChatActivity.kt
+++ app/src/main/java/com/example/practicasoporte/ChatActivity.kt
@@ -1,11 +1,4 @@
 package com.example.practicasoporte
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class ChatActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
-    }
+class ChatActivity {
 }
