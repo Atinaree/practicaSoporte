ab7fe32a75593429a1abead7c2bc2d1272d4cfad
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-31 09:02:16 +0200

    Creación de los diferentes intent para los botones de la catividad de centroAyudaActivity.kt

--- app/src/main/java/com/example/practicasoporte/centroAyudaActivity.kt
+++ app/src/main/java/com/example/practicasoporte/centroAyudaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practicasoporte
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practicasoporte.databinding.ActivityCentroAyudaBinding
@@ -14,6 +15,21 @@ class centroAyudaActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
+        binding.centroAyudaBtnPreguntasFrecuentes.setOnClickListener {
+            val intent = Intent(this, PreguntasFrecuentesActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.centroAyudaBtnAsistente.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.centroAyudaBtnNotificarAveria.setOnClickListener {
+            val intent = Intent(this, NotificacionAveriasActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
