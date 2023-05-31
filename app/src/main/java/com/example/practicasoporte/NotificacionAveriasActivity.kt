d4119a06561d32a91754b7657f46e41bbeceb365
InfiniteNel <juanrolaGame@gmail.com>
2023-05-31 13:01:01 +0200

    31/05/2023 - NotificacionAveriasActivity spinner tipo averia

--- app/src/main/java/com/example/practicasoporte/NotificacionAveriasActivity.kt
+++ app/src/main/java/com/example/practicasoporte/NotificacionAveriasActivity.kt
@@ -2,10 +2,24 @@ package com.example.practicasoporte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.practicasoporte.databinding.ActivityNotificacionAveriasBinding
 
 class NotificacionAveriasActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityNotificacionAveriasBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notificacion_averias)
+        binding = ActivityNotificacionAveriasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val aaAveria = ArrayAdapter<String>(this,
+            android.R.layout.simple_spinner_dropdown_item)
+
+        aaAveria.addAll(arrayListOf(
+            baseContext.resources.getString(R.string.activity_notificacion_averias_spinnerItem1),
+            baseContext.resources.getString(R.string.activity_notificacion_averias_spinnerItem2),
+            baseContext.resources.getString(R.string.activity_notificacion_averias_spinnerItem3)))
+
+        binding.spTipoAveria.adapter = aaAveria
     }
 }
